201a42b62e46ed2779694686c41a470a49bc5222
Barbara Bickham <blbickham@gmail.com>
2013-07-20 20:12:35 -0700

    Hello World Update

--- Hello/src/com/practicejava/lesson2/HelloWorld.java
+++ Hello/src/com/practicejava/lesson2/HelloWorld.java
@@ -3,14 +3,9 @@ package com.practicejava.lesson2;
 public class HelloWorld {
 
 	/**
-	 * @param args
+	 * @param args command line args
 	 */
 	public static void main(String[] args) {
-		
-		String text = "1,234,890"; // example String
-		double value = Double.parseDouble(text.replaceAll("[^0-9.]", ""));
-		
-		// TODO Auto-generated method stub
 		System.out.println("Hello World");
 	}
 
